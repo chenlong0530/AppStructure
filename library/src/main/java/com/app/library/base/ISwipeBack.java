ce006b3562b375c86edbf67626675ba679d963c1
chenlong <chenlong523@pingan.com.cn>
2017-01-17 19:59:00 +0800

    Add utils  classes. 引用https://github.com/Blankj/AndroidUtilCode

--- library/src/main/java/com/app/library/base/ISwipeBack.java
+++ library/src/main/java/com/app/library/base/ISwipeBack.java
@@ -10,12 +10,12 @@ public interface ISwipeBack {
     /**
      * @return the SwipeBackLayout associated with this activity.
      */
-    public abstract SwipeBackLayout getSwipeBackLayout();
+    SwipeBackLayout getSwipeBackLayout();
 
-    public abstract void setSwipeBackEnable(boolean enable);
+    void setSwipeBackEnable(boolean enable);
 
     /**
      * Scroll out contentView and finish the activity
      */
-    public abstract void scrollToFinishActivity();
+    void scrollToFinishActivity();
 }
